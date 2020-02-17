fb5b8e0d1403e0191bfd06e73ff69c16fc6739c7
Appsius <ilyas.ustunkaya234434@gmail.com>
2020-02-17 19:57:00 +0300

    onTick and onComplete

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ class Timer {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+
     if (callbacks) {
       this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
     }
 
     this.startButton.addEventListener('click', this.start);
@@ -26,8 +29,14 @@ class Timer {
   tick = () => {
     if (this.timeRemaining <= 0) {
       this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
     } else {
       this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
     }
   };
 
@@ -48,6 +57,10 @@ const interval = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
     console.log('Timer started!');
   },
-  onTick() {},
-  onComplete() {}
+  onTick() {
+    console.log('Timer is ticking down!');
+  },
+  onComplete() {
+    console.log('Timer completed!');
+  }
 });
